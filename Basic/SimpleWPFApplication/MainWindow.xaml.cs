07b61ae97058fe28ffe93a3733072354dc730f66
Iryna Khromychkina <ikhromychkina@gmail.com>
2020-10-02 20:26:01 -0700

    Created simple WPF application.

---
+++ Basic/SimpleWPFApplication/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace Simple.WPF.App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MoveText_Click(object sender, RoutedEventArgs e)
+        {
+            Target.Text = Source.Text;
+            Source.Text = "";
+        }
+    }
+}
