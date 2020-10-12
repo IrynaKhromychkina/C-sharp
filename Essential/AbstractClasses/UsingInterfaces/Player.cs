a5b43505c76608c92b1888048922e777315fec57
Iryna Khromychkina <ikhromychkina@outlook.com>
2020-10-11 22:53:45 -0700

    3. Created a program with two interfaces for recording and playing audio.

---
+++ Essential/AbstractClasses/UsingInterfaces/Player.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingInterfaces
+{
+    public class Player : IRecordable, IPlayable
+    {
+        public void Play()
+        {
+            Console.WriteLine("Audio is playing");
+        }
+        
+        void IPlayable.Pause()
+        {
+            Console.WriteLine("Audio is paused");
+        }
+
+        void IPlayable.Stop()
+        {
+            Console.WriteLine("Audio stopped");
+        }
+
+         void IRecordable.Record()
+        {
+            Console.WriteLine("Audio is recording");
+        }
+
+        void IRecordable.Pause()
+        {
+            Console.WriteLine("Audio is paused");
+        }
+
+        void IRecordable.Stop()
+        {
+            Console.WriteLine("Audio stopped");
+        }
+    }
+}
---
+++ Essential/AbstractClasses/UsingInterfaces/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingInterfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            IPlayable music = player;
+            music.Play();
+            music.Pause();
+            music.Stop();
+
+            IRecordable recorder = player;
+            recorder.Record();
+            recorder.Pause();
+            recorder.Stop();
+
+            Console.ReadKey();
+        }
+    }
+}
