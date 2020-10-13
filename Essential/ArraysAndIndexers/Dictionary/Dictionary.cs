2c309f8d32e164763f4c2c89fc04722bdf9dae55
Iryna Khromychkina <ikhromychkina@outlook.com>
2020-10-12 20:54:44 -0700

    Practicing with arrays and indexers 1. Created a program English-Russian-Ukrainian doctionary, where you can enter a word in Russian, English or Ukrainian and get the translation in two other languages.

---
+++ Essential/ArraysAndIndexers/Dictionary/Dictionary.cs
@@ -0,0 +1,49 @@
+﻿namespace Dictionary
+{
+    class Dictionary
+    {
+        private string[] valueEnglish = new string[5];
+        private string[] valueRussian = new string[5];
+        private string[] valueUkrainian = new string[5];
+
+        public Dictionary()
+        {
+            valueEnglish[0] = "книга"; valueRussian[0] = "book"; valueUkrainian[0] = "книга";
+            valueEnglish[1] = "ручка"; valueRussian[1] = "pen"; valueUkrainian[1] = "ручка";
+            valueEnglish[2] = "солнце"; valueRussian[2] = "sun"; valueUkrainian[2] = "сонце";
+            valueEnglish[3] = "яблоко"; valueRussian[3] = "apple"; valueUkrainian[3] = "яблуко";
+            valueEnglish[4] = "стол"; valueRussian[4] = "table"; valueUkrainian[4] = "стiл";
+        }
+
+        public string this[string index]
+        {
+            get
+            {
+                for (int i = 0; i < valueEnglish.Length; i++)
+                {
+                    if (valueEnglish[i] == index)
+                        return valueEnglish[i] + " - " + valueRussian[i] + " - " + valueUkrainian[i];
+
+                    if (valueRussian[i] == index)
+                        return valueRussian[i] + " - " + valueEnglish[i] + " - " + valueUkrainian[i];
+
+                    if (valueUkrainian[i] == index)
+                        return valueUkrainian[i] + " - " + valueRussian[i] + " - " + valueEnglish[i];
+                }
+
+                return string.Format("{0} - no translation found.", index);
+            }
+        }
+
+        public string this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < valueEnglish.Length)
+                    return valueEnglish[index] + " - " + valueRussian[index] + " - " + valueUkrainian[index];
+                else
+                    return "Attempt to access an array element out of the array bounds.";
+            }
+        }
+    }
+}
---
+++ Essential/ArraysAndIndexers/Dictionary/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary dictionary = new Dictionary();
+
+            Console.WriteLine(dictionary["book"]);
+            Console.WriteLine(dictionary["house"]);
+            Console.WriteLine(dictionary["pen"]);
+            Console.WriteLine(dictionary["стiл"]);
+            Console.WriteLine(dictionary["pencil"]);
+            Console.WriteLine(dictionary["яблуко"]);
+            Console.WriteLine(dictionary["sun"]);
+
+            Console.WriteLine(new string('-', 20));
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(dictionary[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
