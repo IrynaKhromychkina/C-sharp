bb108ce63baf6265756b6f302b6a1f4b3c90d7ba
Iryna Khromychkina <ikhromychkina@outlook.com>
2020-10-29 21:43:02 -0700

    Using Delegates and Events in WPF applications:  - stopwatch application  - calculator application  - example of using MV pattern

---
+++ Essential/Events/Calculator/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Windows;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public delegate double OperationsDelegate(int a, int b);
+
+        public static double Add(int a, int b) => a + b;
+        public static double Sub(int a, int b) => a - b;
+        public static double Div(int a, int b) => a / (double)b;
+        public static double Mul(int a, int b) => a * b;
+
+        public OperationsDelegate GetUserOperation()
+        {
+            OperationsDelegate operation = null;
+
+            if (AdditionRadioButton.IsChecked.HasValue && AdditionRadioButton.IsChecked.Value)
+            {
+                operation = MainWindow.Add;
+            }
+
+            if (SubtractionRadioButton.IsChecked.HasValue && SubtractionRadioButton.IsChecked.Value)
+            {
+                operation = MainWindow.Sub;
+            }
+
+            if (DivisionRadioButton.IsChecked.HasValue && DivisionRadioButton.IsChecked.Value)
+            {
+                operation = MainWindow.Div;
+            }
+
+            if (MultiplicationRadioButton.IsChecked.HasValue && MultiplicationRadioButton.IsChecked.Value)
+            {
+                operation = MainWindow.Mul;
+            }
+            return operation;
+        }
+        private void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            int intA;
+            int intB;
+            string stringA = ATextBox.Text;
+            string stringB = BTextBox.Text;
+            bool isAParsed = Int32.TryParse(stringA, out intA);
+            bool isBParsed = Int32.TryParse(stringB, out intB);
+
+            if (isAParsed && isBParsed)
+            {
+                OperationsDelegate operation = GetUserOperation();
+
+                if (operation == Div && intB == 0)
+                {
+                    MessageBox.Show("Division by zero is prohibited!");
+                    return;
+                }
+
+                double result = operation(intA, intB);
+
+                ResultTextBox.Text = result.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Incorrect input");
+            }
+        }
+    }
+}
---
+++ Essential/Events/Stopwatch/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows;
+
+namespace Stopwatch
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DateTime startTime;
+        private System.Timers.Timer aTimer;
+        TimeSpan totalTime;
+        public MainWindow()
+        {
+            InitializeComponent();
+            aTimer = new System.Timers.Timer(100);
+            aTimer.Elapsed += OnTimerElapsed;
+            aTimer.AutoReset = true;
+            totalTime = TimeSpan.Zero;
+        }
+
+        private void OnTimerElapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            totalTime += TimeSpan.FromMilliseconds(100);
+
+            Display.Dispatcher.Invoke(() =>
+            {
+                Display.Content = totalTime.TotalSeconds.ToString();
+            });
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            startTime = DateTime.Now;
+            aTimer.Enabled = true;
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            aTimer.Enabled = false;
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            totalTime = TimeSpan.Zero;
+            aTimer.Enabled = false;
+            Display.Content = string.Empty;
+        }
+    }
+}
