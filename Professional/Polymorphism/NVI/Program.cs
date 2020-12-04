3cd38b6223f805269433e90eac7257f2822d9e78
IrynaKhromychkina <ikhromychkina@gmail.com>
2020-12-03 17:17:06 -0800

    Added simple NVI pattern example and simple Template method pattern example

---
+++ Professional/Polymorphism/NVI/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NVI
+{
+    public class Human
+    {
+        public virtual void PhysicalAbilities()
+        {
+            GetUp();
+            BrushTeeth();
+            EatBreakfast();
+        }
+
+        protected virtual void GetUp()
+        {
+            Console.WriteLine("Good morning, I got up!");
+        }
+
+        protected virtual void BrushTeeth()
+        {
+            Console.WriteLine("I brushed my teeth");
+        }
+
+        protected virtual void EatBreakfast()
+        {
+            Console.WriteLine("I ate my breakfast");
+        }
+
+    }
+
+    public class Student : Human
+    {
+        protected override void EatBreakfast()
+        {
+            Console.WriteLine("Sometimes I forgot to eat my breakfast, because I'm a student and always in a hurry");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human Johnson = new Student();
+            Johnson.PhysicalAbilities();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Professional/Polymorphism/TemplateMethod/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace TemplateMethod
+{
+    abstract class PlantingFlowers
+    {
+        public void Planting()
+        {
+            Dig();
+            Plant();
+            PourWater();
+        }
+
+        protected void Dig()
+        {
+            Console.WriteLine("Dig the ground and prepare the place for planting");
+        }
+
+        protected abstract void Plant();
+
+        protected virtual void PourWater()
+        {
+            Console.WriteLine("Pour one cup of water and put a bit of ground on the top");
+        }
+    }
+
+    class Peony : PlantingFlowers
+    {
+        protected override void Plant()
+        {
+            Console.WriteLine("Put the peony plant with root into the hole");
+        }
+
+        protected override void PourWater()
+        {
+            Console.WriteLine("Pour three cups of water and put a bit of ground mixed with compost on the top");
+        }
+    }
+
+    class Tulip : PlantingFlowers
+    {
+        protected override void Plant()
+        {
+            Console.WriteLine("Put the tulip bulb into the hole");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Peony planting instructions");
+            Peony redPeony = new Peony();
+            redPeony.Planting();
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Tulip planting instructions");
+
+            Tulip velvetTulip = new Tulip();
+            velvetTulip.Planting();
+
+            Console.ReadKey();
+        }
+    }
+}
