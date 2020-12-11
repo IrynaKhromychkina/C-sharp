e2d94be90a8ec17dd3a23bcf520a2c37d795f951
IrynaKhromychkina <ikhromychkina@gmail.com>
2020-12-10 19:21:41 -0800

    Threads:  - 3 different threads are working with one method alternatively  - simple program where two threads read he information from two different files simultaneously and write this info on one file simultaneously as well

---
+++ Professional/Threads/ManagingThreads/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace ManagingThreads
+{
+    class Program
+    {
+        static int counter;
+        static object block = new object();
+
+        static void ShowCounter()
+        {
+            lock (block)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    counter++;
+                    Console.WriteLine($"Thread {Thread.CurrentThread.GetHashCode()} is working. Counter = {counter}");
+                }
+                Console.WriteLine($"Thread {Thread.CurrentThread.GetHashCode()} has finished its work\n");
+            }
+        }
+        static void Main(string[] args)
+        {
+            int threadsQuantity = 3;
+            Thread[] threads = new Thread[threadsQuantity];
+            for (int i = 0; i < threadsQuantity; i++)
+            {
+                threads[i] = new Thread(ShowCounter);
+                threads[i].Start();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Professional/Threads/ThreadsWorkingWithFiles/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadsWorkingWithFiles
+{
+    class Program
+    {
+
+        static object block = new object();
+        static string resultFileName = "Joke.txt";
+
+        static void Read(string path)
+        {
+            if (File.Exists(path))
+            {
+                string[] content = File.ReadAllLines(path, Encoding.UTF8);
+                Write(content);
+            }
+        }
+
+        static void Write(string[] content)
+        {
+
+            lock (block)
+            {
+                using (FileStream fileStream = new FileStream(resultFileName, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+                {
+                    fileStream.Seek(0, SeekOrigin.End);
+                    using (var writer = new StreamWriter(fileStream, Encoding.UTF8))
+                    {
+                        for (int i = 0; i < content.Length; i++)
+                        {
+                            writer.WriteLine(content[i]);
+                        }
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            string[] filePaths = new string[] {
+                @"C:\Sources\csharp\Professional\Threads\ThreadsWorkingWithFiles\bin\Debug\FirstDoc.txt",
+                @"C:\Sources\csharp\Professional\Threads\ThreadsWorkingWithFiles\bin\Debug\SecondDoc.txt"};
+
+            var threads = filePaths.Select(fn => new Thread(() => { Read(fn); })).ToArray();
+
+            foreach (Thread thread in threads)
+            {
+                thread.Start();
+            }
+
+            foreach (Thread thread in threads)
+            {
+                thread.Join();
+            }
+
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+    }
+}
