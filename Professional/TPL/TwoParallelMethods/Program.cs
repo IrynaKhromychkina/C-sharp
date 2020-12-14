e89c5dd4bda3abafffca5f82390604b54ff99937
IrynaKhromychkina <ikhromychkina@gmail.com>
2020-12-13 17:20:58 -0800

    TPL - Using parallel LINQ to find all odd numbers in the array - working with two parallel methods and method Main continue working

---
+++ Professional/TPL/TwoParallelMethods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TwoParallelMethods
+{
+    class Program
+    {
+        static void FirstTask()
+        {
+            Thread.Sleep(3000);
+            Console.WriteLine($"Parallel method FirstTask. Thread: {Thread.CurrentThread.ManagedThreadId}");
+        }
+        static void SecondTask()
+        {
+            Thread.Sleep(2000);
+            Console.WriteLine($"Parallel method SecondTask. Thread: {Thread.CurrentThread.ManagedThreadId}");
+        }
+        static void Main(string[] args)
+        {
+            Task firstTask = Task.Factory.StartNew(FirstTask);
+            Task secondTask = Task.Factory.StartNew(SecondTask);
+
+            Console.WriteLine($"Main method finished working. Thread: {Thread.CurrentThread.ManagedThreadId}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Professional/TPL/UsingPLINQ/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace UsingPLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte [] array = new byte [1000000000];
+            Random random = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = (byte)random.Next(0, 100);
+            }
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            ParallelQuery <byte> numbers = from number in array.AsParallel()
+                                         where (number % 2) == 1
+                                         select number;
+
+            int count = numbers.Count();
+
+            stopwatch.Stop();
+
+            //foreach (int number in numbers)
+            //    Console.Write(number + " ");
+
+            Console.WriteLine($"PLINQ has been working for {stopwatch.ElapsedMilliseconds} ms and it found {count} odd numbers");
+
+            Console.ReadKey();
+        }
+    }
+}
